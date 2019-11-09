0195b0dd2ea1f35849abac36302c24dee0e9a4f9
Neciu Dan <dan@adoreme.com>
2019-11-09 10:32:59 -0500

    finished map

--- medijobs-theme/company.php
+++ medijobs-theme/company.php
@@ -104,9 +104,11 @@ get_header();
         <div class="mj-container">
             <div class="mj-grid">
                 <div class="grid__item width-1/24"></div>
-                <div class="grid__item width-10/24">
+                <div class="grid__item width-6/24">
                     <div class="above_headline">Beneficii</div>
                     <h1 class="headline light">Prima platformă de recrutare medicală care ajută companiile să ajungă la specialiștii medicali din România. </h1>
+                </div>
+                <div class="grid__item width-7/24">
                     <ul class="mj-list">
                         <li class="mj-list__item">
                             Aici ce text punem?
@@ -127,6 +129,10 @@ get_header();
     </div>
     <div id="section_3" class="section section_3">
         <div class="mj-container">
+            <div class="background_title hide-on-mobile">
+                OUR Key
+                positions
+            </div>
             <div class="mj-grid">
                 <div class="grid__item width-1/24"></div>
                 <div class="grid__item width-13/24">
@@ -173,7 +179,7 @@ get_header();
                 <div class="grid__item width-10/24">
                     <div class="above_headline">UNDE SUNTEM ACTIVI</div>
                     <h1 class="headline light">Avem peste 33000 de specialiști medicali în cele mai mari orașe din România, iar comunitatea MEDIjobs este în creștere. </h1>
-                    <div class="mj-cols">
+                    <div class="mj-cols hide-on-desktop">
                         <div class="mj-col">
                             <p>Târgul Mureș</p>
                             <p>Galați</p>
@@ -204,10 +210,11 @@ get_header();
                 <div class="grid__item width-1/24"></div>
                 <div class="grid__item width-13/24">
                     <div class="above_headline">Peste</div>
-                    <h1 class="headline uppercase"><span>1200 de companii</span></h1>
-                    <h1 class="headline light" style="margin-top:-10px;">Au apelat la MEDIjobs pentru a angaja cu succes specialisti medical.</h1>
+                    <h1 class="headline biggest uppercase"><span>1200 de companii</span></h1>
+                    <h1 class="headline light">Au apelat la MEDIjobs pentru a angaja cu succes specialisti medical.</h1>
                     <div class="mj-image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/companies_mockup.png" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/companies_mockup.png" class="hide-on-desktop" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/companies_mockup_desktop.png" class="hide-on-mobile" alt="">
                     </div>
                 </div>
             </div>
@@ -404,7 +411,7 @@ get_header();
                 <div class="grid__item width-1/24"></div>
                 <div class="grid__item width-14/24">
                     <div class="quote">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/cnbc.png" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/cnbc.png" style="margin-top:-20px;" alt="">
                         <p>
                             Dr. Wargha Enayati: „Cea mai mare platformă de recrutare medicală din România"
                         </p>
