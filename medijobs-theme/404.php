ae2d8d030b63f76b328f4234eafd735903de384f
Neciu Dan <cst2989@gmail.com>
2020-01-30 16:51:12 +0200

    Added not found and service pages

--- medijobs-theme/404.php
+++ medijobs-theme/404.php
@@ -1,14 +1,29 @@
 <?php get_header(); ?>
-<main id="content">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php esc_html_e( 'Not Found', 'blankslate' ); ?></h1>
-</header>
-<div class="entry-content">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
+
+ <div id="section_1" class="section section_1">
+    <div class="mj-container">
+    	<div class="mj-grid">
+			<div class="grid__item width-1/24 hide-on-mobile"></div>
+			<div class="grid__item width-9/24">
+			    <div class="above_headline"> OOOPS!</div>
+			    <h1 class="headline uppercase">WRONG <br> DIAGNOSIS. </h1>
+			    <p>
+			    	Something went kinda wrong and we couldn’t find the page that were looking for.
+			    	<br><br>
+					There are two options here:
+			    </p>
+			    <div class="links-inline">
+			    	<a href="/" class="mj-btn mj-btn--secondary">Inapoi</a>
+			    	<a href="/site-map" class="mj-btn mj-btn--secondary mj-btn--alt--2">CHECKOUT OUR SITEMAP</a>
+			    </div>
+			    <p class="section_footer">Medijobs Team thanks you for understanding.</p>
+			</div>
+			<div class="grid__item width-6/24 hide-on-mobile">
+			    <div class="secton_bg_image not_found">
+			        <img src="<?php echo get_template_directory_uri(); ?>/images/not-found.png" alt="">
+			    </div>
+			</div>
+		</div>
+	</div>
 </div>
-</article>
-</main>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
